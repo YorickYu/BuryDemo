508434d12121ce5cbc792759439a4c83859a64c9
YY <yy@ioneball.com>
2019-12-06 12:42:08 +0800

    git commit -2

--- Bury/AppDelegate.m
+++ Bury/AppDelegate.m
@@ -18,6 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     // hehe
+    // haha
     return YES;
 }
 
